bb9df59db1809b7ea9f98ad24af1a4b632c18ac1
muchj <muchj@onid.oregonstate.edu>
2016-04-13 05:01:43 -0700

    Create randInts.cpp

---
+++ randInts.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+#include <ctime>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int min, max;
+	long total;
+	string fileName;
+	ofstream outputFile;
+	unsigned seed;
+
+	seed = time(0);
+	srand(seed);
+
+	cout << "How many randoms? ";
+	cin >> total; // Number of randoms.
+
+	cout << "What is the minimum? ";
+	cin >> min; // Minimum int, can be negative.
+
+	cout << "What is the maximum? ";
+	cin >> max; // Maximum int.
+
+	cin.get();
+	cout << "Into which file? ";
+	getline(cin, fileName);
+	
+	outputFile.open(fileName.c_str());
+	
+	int num;
+	for (long i = 0; i < total; i++)
+	{
+		num = rand() % (max - min + 1) + min;
+		outputFile << num;
+		outputFile << " ";
+
+		if ((i + 1)% 20 == 0) // This just makes the file look nicer.
+			outputFile << "\n";
+	}
+
+	outputFile.close();
+	
+	return 0;
+}
