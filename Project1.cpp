cb0d3e028ec355720259e2ce15286a8f67bcd833
Adam Much <much.adam@gmail.com>
2016-04-16 05:49:10 -0700

    Changed output to accommodate structs

--- Project1.cpp
+++ Project1.cpp
@@ -9,15 +9,8 @@ Instructions:
 #include <istream>
 #include <string>
 #include <algorithm>
-using namespace std;
 
-void output(int* array, int arrSize, int sum, int startIdx, int endIdx);
-////////////////////////////////////////////
-// START - Algorithms
-////////////////////////////////////////////
-////////////////////////////////////////////
-// Algorithm 1 - Enumeration
-////////////////////////////////////////////
+using namespace std;
 
 struct results{
     int sum;
@@ -25,7 +18,211 @@ struct results{
     int endIdx;
 };
 
-results mssAlgorithm1(int* array, int size){
+void output(int* array, int arrSize, int sum, int startIdx, int endIdx, int algNo);
+int inputFileLineCount();
+void parseInputFile(int lineCount);
+void testCorrectness();
+results mssAlgorithm1(int* array, int size);
+results mssAlgorithm2(int* array, int size);
+results mssAlgorithm3(int* array, int f, int l);
+results mssAlgorithm4(int* array, int size);
+
+////////////////////////////////////////////
+// MAIN
+////////////////////////////////////////////
+int main(int argc, char *argv[]){
+	if( argc > 1)
+	{
+		/*if(argv[1] == "-r") //Testing Run Time
+		{
+			testRunTime();
+		}*/
+		if(argv[1] == "-c") //Testing Correctness
+		{
+			testCorrectness();
+		}
+		/*else if(argv[1] == "-e") //Testing Example
+		{
+			testExample();
+		}*/
+		else
+		{
+			cout << "You did not enter correct arguments.\n" << endl;
+			cout << "Please add -r,-c, or -e as an argument." << endl;
+		}
+	}
+	else
+	{
+		testCorrectness(); //This will default to test correctness if not enough arguments are entered.	
+	}
+	return 0;
+}
+
+////////////////////////////////////////////
+//START - Programs
+////////////////////////////////////////////
+void output(int* array, int arrSize, int sum, int startIdx, int endIdx, int algNo)
+{
+    ofstream out;
+    
+    out.open("MSS_Results.txt", ofstream::out | ofstream::app);
+    
+	out << "Algorithm " << algNo << ":\n";
+	cout << "Algorithm " << algNo << ":\n";
+
+    for(int i = 0; i < arrSize; i++)
+    {
+        out << array[i] << ' ';
+		cout << array[i] << ' ';
+    }
+    out << '\n';
+	cout << '\n';
+    
+    for(int i = startIdx; i <= endIdx; i++)
+    {
+        out << array[i] << ' ';
+		cout << array[i] << ' ';
+    }
+    out << '\n';
+	cout << '\n';
+    
+    out << sum << '\n';
+	cout << sum << '\n';
+
+	out << '\n';
+	cout << '\n';
+    
+    out.close();    
+}
+////////////////////////////////////////////
+//	inputFileLineCount
+//	- Gets line count of MSS_Problems.txt
+////////////////////////////////////////////
+int inputFileLineCount()
+{
+	int lineCount = 0;
+	string line;
+	ifstream inputFile("MSS_Problems.txt");
+	while (inputFile.good())
+	{
+		getline(inputFile, line);
+		//cout << line << endl;
+		if(line == "")
+		{
+			break;
+		}
+		else
+		{
+			lineCount++;
+		}
+	}
+	//cout << "Number of lines is " << lineCount << endl;
+	return lineCount;
+}
+////////////////////////////////////////////
+// 	parseInputFile
+//	- Is THE function called that is used for grading
+//	- It reads in MSS_Problems.txt
+//	- Crunches the sum for all four algorithms
+//	- Outputs the result onto the screen AND MSS_Results.txt for grading 
+////////////////////////////////////////////
+void parseInputFile(int lineCount)
+{
+	char comma = ',';
+	char leftBracket = '[';
+	char rightBracket = ']';
+	
+	ifstream inputFile("MSS_Problems.txt");
+	ofstream outputFile("tempFile"); // Used for testing.
+	char c;
+	int nextInt;
+	
+	int A[lineCount][100]; // Make lineCount number of arrays.
+
+	int arraySize[lineCount]; // Corresponding array sizes.
+
+	int size = 0; // current size of one of the arrays.
+	int i = 0; // Incremental number for line (array) from input lineCount.
+	int j = 0; // A[i][j]
+	
+	if (inputFile && outputFile)
+	{
+		while(!inputFile.eof())
+		{
+			inputFile >> c;
+			if (c == comma || c == leftBracket)// We know an int is next.
+			{					
+				inputFile >> nextInt;
+
+				// Used for testing.
+				outputFile << nextInt;
+				outputFile << ' ';
+				
+				// Plug the int into its spot, and increment the spot and size.
+				A[i][j] = nextInt;
+				//cout << nextInt << "\t" << A[i][j] << endl;
+				j++;
+				size++;
+			}
+
+			if (c == rightBracket) // End of an array.
+			{
+				// Used for testing.
+				outputFile << '\n';
+				
+				// Plug in the array size, reset size to 0, and move to next array.
+				arraySize[i] = size;
+				size = 0;
+				i++;
+				j = 0;
+				
+			}
+			
+		}
+	}
+	inputFile.close();
+	outputFile.close();
+	
+	// Should be able to run algorithms on A[i][] and arraySize[i] here.
+
+	results resOut;
+	for (int k = 0; k < lineCount; k++)
+	{
+		resOut = mssAlgorithm1(A[k], arraySize[k]);
+		output(A[k], arraySize[k], resOut.sum, resOut.startIdx, resOut.endIdx, 1);
+
+		resOut = mssAlgorithm2(A[k], arraySize[k]);
+		output(A[k], arraySize[k], resOut.sum, resOut.startIdx, resOut.endIdx, 2);
+
+		resOut = mssAlgorithm3(A[k], 1, arraySize[k]);
+		output(A[k], arraySize[k], resOut.sum, resOut.startIdx, resOut.endIdx, 3);
+
+		resOut = mssAlgorithm4(A[k], arraySize[k]);
+		output(A[k], arraySize[k], resOut.sum, resOut.startIdx, resOut.endIdx, 4);
+	}
+	//cout << "Now calculating line # " << lineCount + 1 << endl;	
+}
+////////////////////////////////////////////
+//	testCorrectness
+//	- Used for calculating grade
+////////////////////////////////////////////
+void testCorrectness()
+{	
+	parseInputFile(inputFileLineCount());
+}
+////////////////////////////////////////////
+//END - Programs
+////////////////////////////////////////////
+
+////////////////////////////////////////////
+// START - Algorithms
+////////////////////////////////////////////
+
+////////////////////////////////////////////
+// Algorithm 1 - Enumeration
+////////////////////////////////////////////
+results mssAlgorithm1(int* array, int size)
+{
 	int i, j, k, sum;
         struct results r;
         r.sum = array[0];
@@ -48,11 +245,12 @@ results mssAlgorithm1(int* array, int size){
         
         return r;
 }
+
 ////////////////////////////////////////////
 //Algorithm 2
 ////////////////////////////////////////////
-
-results mssAlgorithm2(int* array, int size){
+results mssAlgorithm2(int* array, int size)
+{
 	int i, j, sum;
         results r;
         r.sum = array[0];
@@ -157,6 +355,9 @@ results mssAlgorithm3(int* array, int f, int l)
     return r;   
 }
 
+////////////////////////////////////////////
+//Algorithm 4
+////////////////////////////////////////////
 results mssAlgorithm4(int* array, int size)
 {
     int maxNow = 0; 
@@ -198,148 +399,3 @@ results mssAlgorithm4(int* array, int size)
 ////////////////////////////////////////////
 //END - Algorithms
 ////////////////////////////////////////////
-
-////////////////////////////////////////////
-//START - Programs
-////////////////////////////////////////////
-void output(int* array, int arrSize, int sum, int startIdx, int endIdx)
-{
-    ofstream out;
-    
-    out.open("MSS_Results.txt", ofstream::out | ofstream::app);
-    
-    for(int i = 0; i < arrSize; i++)
-    {
-        out << array[i] << ' ';
-    }
-    out << '\n';
-    
-    for(int i = startIdx; i <= endIdx; i++)
-    {
-        out << array[i] << ' ';
-    }
-    out << '\n';
-    
-    out << sum << '\n';
-    
-    out.close();    
-}
-////////////////////////////////////////////
-//	inputFileLineCount
-//	- Gets line count of MSS_Problems.txt
-////////////////////////////////////////////
-int inputFileLineCount(){
-	int lineCount = 0;
-	string line;
-	ifstream inputFile("MSS_Problems.txt");
-	while (inputFile.good())
-	{
-		getline(inputFile, line);
-		cout << line << endl;
-		if(line == "")
-		{
-			break;
-		}
-		else
-		{
-			lineCount++;
-		}
-	}
-	cout << "Number of lines is " << lineCount << endl;
-	return lineCount;
-}
-////////////////////////////////////////////
-// 	parseInputFile
-//	- Is THE function called that is used for grading
-//	- It reads in MSS_Problems.txt
-//	- Crunches the sum for all for algorithms
-//	- Outputs the result onto the screen AND MSS_Results.txt for grading 
-////////////////////////////////////////////
-void parseInputFile(int lineCount){
-		string line;
-		string delimiter = ", ";
-		
-		ifstream inputFile("MSS_Problems.txt");
-		if (inputFile)
-		{
-			while(getline(inputFile, line))
-			{
-				//cout << line << endl;
-				
-			}
-		}
-		cout << "Now calculating line # " << lineCount +1 << endl;	
-}
-////////////////////////////////////////////
-//	testCorrectness
-//	- Used for calculating grade
-////////////////////////////////////////////
-void testCorrectness()
-{
-	int numberOfArraysForCalc = inputFileLineCount();
-	
-	int i = 0;
-	while(i < numberOfArraysForCalc)
-	{
-		parseInputFile(i);
-		i += 1;
-	}
-}
-////////////////////////////////////////////
-//END - Programs
-////////////////////////////////////////////
-
-////////////////////////////////////////////
-// MAIN
-////////////////////////////////////////////
-int main(int argc, char *argv[]){
-	if( argc > 1)
-	{
-		/*if(argv[1] == "-r") //Testing Run Time
-		{
-			testRunTime();
-		}*/
-		if(argv[1] == "-c") //Testing Correctness
-		{
-			testCorrectness();
-		}
-		/*else if(argv[1] == "-e") //Testing Example
-		{
-			testExample();
-		}*/
-		else
-		{
-			cout << "You did not enter correct arguments.\n" << endl;
-			cout << "Please add -r,-c, or -e as an argument." << endl;
-		}
-	}
-	else
-	{
-		testCorrectness(); //This will default to test correctness if not enough arguments are entered.	
-	}
-	return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
